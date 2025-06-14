a7f40f057d5f306e3686f19c39ec2f88efc66f1c
lahcen404 <lahcen.maskour2003@gmail.com>
2025-06-15 00:34:43 +0100

    Create Project Fullstack

---
+++ DeliveryMatch-BackEnd/src/test/java/com/deliveryMatch/DeliveryMatch/DeliveryMatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deliveryMatch.DeliveryMatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryMatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
